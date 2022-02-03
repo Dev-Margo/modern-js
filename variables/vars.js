50ffcde22cf57dcb8d97124f9e838d3e81f009cb
Margo Stepka <tstepka@gmail.com>
2022-02-02 19:46:29 -0500

    chapter 2 variables section tasks

---
+++ variables/vars.js
@@ -0,0 +1,14 @@
+//task 1
+let admin;
+let name;
+name = "john";
+admin = name;
+console.log(admin);
+
+//task 2
+let thirdRockFromTheSun = 'earth';
+let currentUser = 'rando';
+
+//task 3
+//uppercase constants are for hard coded hard to remember values like hex codes.
+//dont use for age since its the output of a function not something thats hardcoded.
